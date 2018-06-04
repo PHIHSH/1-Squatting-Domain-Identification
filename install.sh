364a1d8e2c86adbbe13acfd9e9ae93a25026f09c
KeTian <ririhedou@gmail.com>
2018-06-04 12:49:44 -0400

    add install

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "check your python and pip version first"
+echo "Passed version is Python 3 and pip3"
+python3 --version
+pip3 --version
+
+echo "Install dependences"
+
+echo "Make the urlCrazy executable"
+chmod +x ./squatting/urlcrazy-0.5/urlcrazy
+
+
+
+echo "Done!"
